fd1f0ced209b6a20658cc70299f65d8ded9ecac7
Will Galebach <willbach@gmail.com>
2022-07-21 17:26:21 +0200

    fixing App.ts

--- ui/src/app.tsx
+++ ui/src/app.tsx
@@ -1,10 +1,11 @@
 import React, { useEffect } from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Navbar from './components/nav/Navbar';
-import EditorView from './views/EditorView';
-import DocsView from './views/DocsView';
-import CliffView from './views/CliffView';
 import useContractStore from './store/contractStore';
+import Container from './components/spacing/Container';
+import Col from './components/spacing/Col';
+import Row from './components/spacing/Row';
+import { Sidebar } from './components/nav/Sidebar';
+import MainView from './views/MainView';
 
 function App() {
   const { init } = useContractStore()
@@ -14,23 +15,34 @@ function App() {
   }, []) // eslint-disable-line react-hooks/exhaustive-deps
 
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<EditorView />} />
-        <Route path="/gall" element={<EditorView />} />
-        <Route path="/docs" element={<DocsView />} />
-        <Route path="/cliff" element={<CliffView />} />
-        <Route
-          path="*"
-          element={
-            <main style={{ padding: "1rem" }}>
-              <p>There's nothing here!</p>
-            </main>
-          }
-        />
-      </Routes>
-    </BrowserRouter>
+    <Container>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <Col style={{ width: '100%', height: '100%' }}>
+        <Row style={{ height: '100%', width: '100%' }}>
+          <Col style={{ height: '100%', width: '20%', maxWidth: 240 }}>
+            <Sidebar />
+          </Col>
+          <Col style={ {minWidth: '80%', width: 'calc(100% - 240px)', height: '100%', position: 'relative' }}>
+            <Routes>
+              <Route path="/" element={<MainView />} />
+              {/* <Route path="/" element={<EditorView />} />
+              <Route path="/new" element={<NewProjectView />} />
+              <Route path="/app" element={<AppView />} />
+              <Route path="/app/:app" element={<AppView />} /> */}
+              <Route
+                path="*"
+                element={
+                  <main style={{ padding: "1rem" }}>
+                    <p>There's nothing here!</p>
+                  </main>
+                }
+              />
+            </Routes>
+          </Col>
+        </Row>
+      </Col>
+      </BrowserRouter>
+    </Container>
   );
 }
 
